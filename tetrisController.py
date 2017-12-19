7399267b73f42c1ba3de5087f75b6ca6b56d7702
segnolin <segnolin@gmail.com>
2017-12-19 11:52:25 +0800

    fixed tetris display issue and coding style adjustment

--- tetrisController.py
+++ tetrisController.py
@@ -1,63 +1,62 @@
-from PyQt5.QtWidgets import (QMainWindow, QFrame, QDesktopWidget, QApplication,
-                             QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QLabel,
-                             QGridLayout)
-from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
-from PyQt5.QtGui import QPainter, QColor , QFont
-import sys, random
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from tetrisView import *
 from tetrisModel import *
+
+import sys, random
 
 class tetrisController(QMainWindow):
 
     def __init__(self):
         super().__init__()
         self.initUI()
+
     def initUI(self):
 
-
         self.window=tetrisView(self)
-       
-        self.currentState=self.window.model.getState()
+        self.currentState=self.window.game.model.getState()
         self.window.startButton.clicked.connect(self.buttonClicked)
         self.window.pauseButton.clicked.connect(self.buttonClicked)
         self.window.exitButton.clicked.connect(self.buttonClicked)
+
     def buttonClicked(self):
+
         sender=self.sender()
-        if sender.text()=="START":
+        if sender.text()=='START':
             self.start()
-        elif sender.text()=="PAUSE":
+        elif sender.text()=='PAUSE':
             self.pause()
-        elif sender.text()=="EXIT":
+        elif sender.text()=='EXIT':
             self.exit()
+
     def start(self):
-        if self.currentState=="START":
+
+        if self.currentState=='START':
             return
-        self.currentState="START"
-        self.window.start()
+        self.currentState='START'
+        self.window.game.start()
+
     def pause(self):
-        if self.currentState!="START":
+
+        if self.currentState!='START':
             return
-        self.currentState="PAUSE"
-        self.window.pause()
+        self.currentState='PAUSE'
+        self.window.game.pause()
+
     def exit(self):
-        self.window.exit()
+
+        self.window.game.exit()
 
     def getUserInput(self, sender):
-        if sender.text()=="START":
+
+        if sender.text()=='START':
             self.start()
-        elif sender.teext()=="PAUSE":
+        elif sender.teext()=='PAUSE':
             self.pause()
-
-
-
-
-
-
-
 
 if __name__ == '__main__':
     
     app = QApplication([])
     tetris = tetrisController()
-    
     sys.exit(app.exec_())
--- tetrisView.py
+++ tetrisView.py
@@ -1,167 +1,202 @@
-from PyQt5.QtWidgets import (QMainWindow, QFrame, QDesktopWidget, QApplication,
-                             QPushButton, QHBoxLayout, QVBoxLayout, QWidget, QLabel,
-                             QGridLayout)
-from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
-from PyQt5.QtGui import QPainter, QColor, QFont
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from tetrisModel import *
+from pyqtwindow import *
+
 import sys, random
-from pyqtwindow import Ui_MainWindow
-
-
 
 class tetrisView(QMainWindow, Ui_MainWindow):
-    BoardWidth=10
-    BoardHeight=22
-    Speed=300
 
     def __init__(self, parent):
         super(tetrisView, self).__init__(parent)
         self.setupUi(self)
+        self.game = tetrisGame(self)
+        self.game.setGeometry(119, 9, 251, 521)
+        self.show()
+
+    def keyPressEvent(self, event):
+
+        if self.game.model.getState()!='START':
+            return
+        key = event.key()
+
+        print('key')
+        if key == Qt.Key_Left:
+            self.statusBar.showMessage('left')
+            self.game.Move(self.game.currentPiece, self.game.curX - 1, self.game.curY)
+        elif key == Qt.Key_Right:
+            self.statusBar.showMessage('right')
+            self.game.Move(self.game.currentPiece, self.game.curX + 1, self.game.curY)
+        elif key == Qt.Key_Down:
+            self.statusBar.showMessage('down')
+            self.game.Move(self.game.currentPiece, self.game.curX, self.game.curY - 1)
+        elif key == Qt.Key_Up:
+            self.statusBar.showMessage('up')
+            self.game.Move(self.game.currentPiece.rotateLeft(), self.game.curX, self.game.curY)
+        elif key == Qt.Key_Space:
+            self.game.dropDown()
+        elif key == Qt.Key_Space:
+            self.statusBar.showMessage('space')
+            self.game.dropDown()
+        elif key == Qt.Key_Escape:
+            self.close()
+
+class tetrisGame(QFrame):
+
+    BoardWidth = 10
+    BoardHeight = 22
+    Speed = 300
+
+    def __init__(self, parent):
+        super().__init__(parent)
         self.initBoard()
         
     def initBoard(self):
-        self.timer=QBasicTimer()
-        self.model=tetrisModel()
-        self.currentPiece=tetrisModel()
-        self.waiting=False
-        self.curX=0
-        self.curY=0
-        self.board=[]
-        self.isStarted=False
-        self.isPaused=False
 
-
+        self.timer = QBasicTimer()
+        self.model = tetrisModel()
+        self.currentPiece = tetrisModel()
+        self.waiting = False
+        self.curX = 0
+        self.curY = 0
+        self.board = []
+        self.isStarted = False
+        self.isPaused = False
         self.resize(380, 600)
         self.setWindowTitle('Tetris')
         self.clearBoard()
-        self.show()
-        
+
     def clearBoard(self):
-        for i in range(tetrisView.BoardWidth * tetredisView.BoardHeight):
-            self.board.append(PiecesShape.NoShape);
+
+        for i in range(tetrisGame.BoardWidth*tetrisGame.BoardHeight):
+            self.board.append(PiecesShape.NoShape)
+            
     def start(self):
-        self.model.setState("START")
-        self.startButton.setEnabled(False)
-        self.pauseButton.setEnabled(True)
-        self.scores.setText("0")
-        self.lines.setText("0")
-        self.statusBar.showMessage("Game start!")  
+
+        self.model.setState('START')
+        self.parent().startButton.setEnabled(False)
+        self.parent().pauseButton.setEnabled(True)
+        self.parent().scores.setText('0')
+        self.parent().lines.setText('0')
+        self.parent().statusBar.showMessage('Game start!')  
         self.clearBoard()
         self.newPiece()
-        self.timer.start(tetrisView.Speed, self)
+        self.timer.start(tetrisGame.Speed, self)
+
     def pause(self):
-        self.model.setState("PAUSE")
-        self.pauseButton.setEnabled(False)
-        self.startButton.setEnabled(True)
-        self.statusBar.showMessage("Game pause!")
+
+        self.model.setState('PAUSE')
+        self.parent().pauseButton.setEnabled(False)
+        self.parent().startButton.setEnabled(True)
+        self.parent().statusBar.showMessage('Game pause!')
         self.timer.stop()
-        self.frame.update()
+        self.update()
+
     def exit(self):
-        self.statusBar.showMessage("Bye!")
-        self.close()
 
-    def keyPressEvent(self, event):
-        if self.model.getState()!="START":
-            return
-        key=event.key()
-
-        if key == Qt.Key_Left:
-            self.statusBar.showMessage("left")
-            self.Move(self.currentPiece, self.curX-1, self.curY)
-        elif key == Qt.Key_Right:
-            self.statusBar.showMessage("right")
-            self.Move(self.currentPiece, self.curX+1, self.curY)
-        elif key == Qt.Key_Down:
-            self.statusBar.showMessage("down")
-            self.Move(self.currentPiece, self.curX, self.curY-1)
-        elif key == Qt.Key_Up:
-            self.statusBar.showMessage("up")
-        elif key == Qt.Key_Space:
-            self.statusBar.showMessage("space")
-            self.dropDown()
-        elif key==Qt.Key_Escape:
-            self.close()
-
-
+        self.parent().statusBar.showMessage('Bye!')
+        self.parent().close()
+        
     def timerEvent(self, event):
-        if event.timerId()==self.timer.timerId():
+
+        if event.timerId() == self.timer.timerId():
             if self.waiting:
-                self.waiting=False
+                self.waiting = False
                 self.newPiece()
             else:
                 self.autoDown()
         else:
-            super(tetrisView, self).timerEvent(event)
+            super(tetrisGame, self).timerEvent(event)
+            
     def newPiece(self):
+         
         self.currentPiece.setRandomShape()
-        self.curX=tetrisView.BoardWidth//2+1
-        self.curY=tetrisView.BoardHeight-1+self.currentPiece.minY()
+        self.curX = tetrisGame.BoardWidth // 2 + 1
+        self.curY = tetrisGame.BoardHeight - 1 + self.currentPiece.minY()
         if not self.Move(self.currentPiece, self.curX, self.curY):
             self.currentPiece.setShape(PiecesShape.NoShape)
             self.timer.stop()
+
     def dropDown(self):
-        newY=self.curY
-        while newY>0:
-            if not self.Move(self.currentPiece, self.curX, newY-1):
+
+        newY = self.curY
+        while newY > 0:
+            if not self.Move(self.currentPiece, self.curX, newY - 1):
                 break
-            newY-=1
+            newY -= 1
         self.pieceDropped()
+
     def autoDown(self):
-        if not self.Move(self.currentPiece, self.curX,self.curY-1):
+
+        if not self.Move(self.currentPiece, self.curX,self.curY - 1):
             self.pieceDropped()
+
     def shapeAt(self, x, y):
-        return self.board[(y*tetrisView.BoardWidth)+x]
-    def setShapeAt(self , x, y, shape):
-        self.board[(y * tetrisView.BoardWidth) + x]=shape
+
+        return self.board[(y * tetrisGame.BoardWidth) + x]
+
+    def setShapeAt(self, x, y, shape):
+
+        self.board[(y * tetrisGame.BoardWidth) + x] = shape
+
     def pieceDropped(self):
+
         for i in range(4):
-            x=self.curX+self.currentPiece.x(i)
-            y=self.curY-self.currentPiece.y(i)
+            x = self.curX + self.currentPiece.x(i)
+            y = self.curY - self.currentPiece.y(i)
             self.setShapeAt(x, y, self.currentPiece.getShape())
         if not self.waiting:
             self.newPiece()
+
     def Move(self, newPiece, newX, newY):
+
         for i in range(4):
-            x=newX+newPiece.x(i)
-            y=newY-newPiece.y(i)
-            if x<0 or x>= tetrisView.BoardWidth or y<0 or y>= tetrisView.BoardHeight:
+            x = newX + newPiece.x(i)
+            y = newY - newPiece.y(i)
+            if x < 0 or x >= tetrisGame.BoardWidth or y < 0 or y >= tetrisGame.BoardHeight + 4:
                 return False
-            if self.shapeAt(x, y) !=PiecesShape.NoShape:
+            if self.shapeAt(x, y) != PiecesShape.NoShape:
                 return False
-        self.currentPiece=newPiece
-        self.curX=newX
-        self.curY=newY
+        self.currentPiece = newPiece
+        self.curX = newX
+        self.curY = newY
         self.update()
         return True
+
     def squareWidth(self):
-        return self.contentsRect().width()//tetrisView.BoardWidth
+
+        return self.contentsRect().width() // tetrisGame.BoardWidth
+
     def squareHeight(self):
-        return self.contentsRect().height()//tetrisView.BoardHeight
+
+        return self.contentsRect().height() // tetrisGame.BoardHeight
+
     def paintEvent(self, event):
         
         painter = QPainter(self)
         print(painter.device())
         rect = self.contentsRect()
         
-        boardTop = rect.bottom() - tetrisView.BoardHeight * self.squareHeight()
+        boardTop = rect.bottom() - tetrisGame.BoardHeight * self.squareHeight()
         
-        for i in range(tetrisView.BoardHeight):
-            for j in range(tetrisView.BoardWidth):
+        for i in range(tetrisGame.BoardHeight):
+            for j in range(tetrisGame.BoardWidth):
                 
-                shape = self.shapeAt(j, tetrisView.BoardHeight - i - 1)
+                shape = self.shapeAt(j, tetrisGame.BoardHeight - i - 1)
                 
                 if shape != PiecesShape.NoShape:
                     self.drawSquare(painter,rect.left() + j * self.squareWidth(),
                                     boardTop + i * self.squareHeight(), shape)
-        if self.currentPiece.getShape() != PiecesShape.NoShape:
+        if (self.currentPiece.getShape() != PiecesShape.NoShape):
               
             for i in range(4):
                 x = self.curX + self.currentPiece.x(i)
                 y = self.curY - self.currentPiece.y(i)
                 self.drawSquare(painter, rect.left() + x * self.squareWidth(),
-                    boardTop + (tetrisView.BoardHeight - y - 1) * self.squareHeight(),
+                    boardTop + (tetrisGame.BoardHeight - y - 1) * self.squareHeight(),
                     self.currentPiece.getShape())
-                
+
     def drawSquare(self, painter, x, y, shape):        
 
         colorTable = [0x000000, 0xCC6666, 0x66CC66, 0x6666CC,
@@ -180,5 +215,3 @@ class tetrisView(QMainWindow, Ui_MainWindow):
             x + self.squareWidth() - 1, y + self.squareHeight() - 1)
         painter.drawLine(x + self.squareWidth() - 1, 
             y + self.squareHeight() - 1, x + self.squareWidth() - 1, y + 1)
-
-
