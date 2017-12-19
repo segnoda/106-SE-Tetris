1902872f39b8ca053fb8ba3a784a8029069c54a4
l9010333 <32293117+l9010333@users.noreply.github.com>
2017-12-19 11:37:31 +0800

    Add buttonClickEvent

--- tetrisController.py
+++ tetrisController.py
@@ -4,21 +4,49 @@ from PyQt5.QtWidgets import (QMainWindow, QFrame, QDesktopWidget, QApplication,
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor , QFont
 import sys, random
-from tetrisView import tetrisView
-from tetrisModel import tetrisModel 
+from tetrisView import *
+from tetrisModel import *
 
 class tetrisController(QMainWindow):
-    
+
     def __init__(self):
         super().__init__()
         self.initUI()
     def initUI(self):
+
+
         self.window=tetrisView(self)
- 
-        
+       
+        self.currentState=self.window.model.getState()
+        self.window.startButton.clicked.connect(self.buttonClicked)
+        self.window.pauseButton.clicked.connect(self.buttonClicked)
+        self.window.exitButton.clicked.connect(self.buttonClicked)
+    def buttonClicked(self):
+        sender=self.sender()
+        if sender.text()=="START":
+            self.start()
+        elif sender.text()=="PAUSE":
+            self.pause()
+        elif sender.text()=="EXIT":
+            self.exit()
+    def start(self):
+        if self.currentState=="START":
+            return
+        self.currentState="START"
+        self.window.start()
+    def pause(self):
+        if self.currentState!="START":
+            return
+        self.currentState="PAUSE"
+        self.window.pause()
+    def exit(self):
+        self.window.exit()
 
-
-
+    def getUserInput(self, sender):
+        if sender.text()=="START":
+            self.start()
+        elif sender.teext()=="PAUSE":
+            self.pause()
 
 
 
