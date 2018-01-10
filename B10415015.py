5c63afe78407948faa091c2569958f202c7de876
nepsuki <bg30827@gmail.com>
2018-01-10 22:50:58 +0800

    add view

---
+++ B10415015.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(460, 580)
+        MainWindow.setStyleSheet("""
+                * {
+                    background: #e0ffff;
+                    color: #4b0082;
+                    font-size: 14px;
+                }
+                QPushButton {
+                    background: #00ffff;
+                    border-radius: 8px;
+                }
+                QPushButton:hover {
+                    background: #1d68d9;
+                }
+                QPushButton#exitButton:hover {
+                    background: #ff6259;
+                }
+                QPushButton:disabled {
+                    color: #7f8081;
+                    background: #87ceeb;
+                }
+                QFrame {
+                    background: #afeeee;
+                    border: 4px solid #1e90ff;
+                }
+                QLabel {
+                    background: #e0ffff;
+                    border: none;
+                }
+                QLabel#scores {
+                    font-size: 20px;
+                }
+                QLabel#lines {
+                    font-size: 20px;
+                }
+                QStatusBar {
+                    background: #1e90ff;
+                }
+                """)
+        self.centralWidget = QtWidgets.QWidget(MainWindow)
+        self.centralWidget.setObjectName("centralWidget")
+        self.startButton = QtWidgets.QPushButton(self.centralWidget)
+        self.startButton.setGeometry(QtCore.QRect(350, 380, 91, 28))
+        self.startButton.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.startButton.setAutoDefault(False)
+        self.startButton.setDefault(False)
+        self.startButton.setFlat(False)
+        self.startButton.setObjectName("startButton")
+        self.pauseButton = QtWidgets.QPushButton(self.centralWidget)
+        self.pauseButton.setGeometry(QtCore.QRect(350, 420, 91, 28))
+        self.pauseButton.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.pauseButton.setObjectName("pauseButton")
+        self.exitButton = QtWidgets.QPushButton(self.centralWidget)
+        self.exitButton.setGeometry(QtCore.QRect(350, 460, 91, 28))
+        self.exitButton.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.exitButton.setObjectName("exitButton")
+        self.score = QtWidgets.QLabel(self.centralWidget)
+        self.score.setGeometry(QtCore.QRect(20, 30, 70, 20))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.score.setFont(font)
+        self.score.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.score.setObjectName("score")
+        self.scores = QtWidgets.QLabel(self.centralWidget)
+        self.scores.setGeometry(QtCore.QRect(20, 60, 60, 15))
+        self.scores.setObjectName("scores")
+        self.line = QtWidgets.QLabel(self.centralWidget)
+        self.line.setGeometry(QtCore.QRect(20, 100, 70, 20))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.line.setFont(font)
+        self.line.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.line.setObjectName("line")
+        self.lines = QtWidgets.QLabel(self.centralWidget)
+        self.lines.setGeometry(QtCore.QRect(20, 140, 60, 15))
+        self.lines.setObjectName("lines")
+        self.frame = QtWidgets.QFrame(self.centralWidget)
+        self.frame.setGeometry(QtCore.QRect(90, 10, 251, 521))
+        self.frame.setFrameShape(QtWidgets.QFrame.Box)
+        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
+        self.frame.setLineWidth(1)
+        self.frame.setMidLineWidth(0)
+        self.frame.setObjectName("frame")
+        MainWindow.setCentralWidget(self.centralWidget)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 460, 21))
+        self.menuBar.setObjectName("menuBar")
+        MainWindow.setMenuBar(self.menuBar)
+        self.statusBar = QtWidgets.QStatusBar(MainWindow)
+        self.statusBar.setObjectName("statusBar")
+        MainWindow.setStatusBar(self.statusBar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.startButton.setText(_translate("MainWindow", "START"))
+        self.pauseButton.setText(_translate("MainWindow", "PAUSE"))
+        self.exitButton.setText(_translate("MainWindow", "EXIT"))
+        self.score.setText(_translate("MainWindow", "SCORE"))
+        self.scores.setText(_translate("MainWindow", "TextLabel"))
+        self.line.setText(_translate("MainWindow", "LINE"))
+        self.lines.setText(_translate("MainWindow", "TextLabel"))
+
